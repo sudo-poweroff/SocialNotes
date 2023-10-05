a2fcea07f9e4b71b09b2e6aff24d553fa05d7fcf
Rocco000 <r.iuliano13@studenti.unisa.it>
2023-10-05 12:26:47 +0200

    [CR2] Realizzato i test cases per la funzionalità ModificaEmail #2

--- test/profilosistema/ModificaEmailTest.java
+++ test/profilosistema/ModificaEmailTest.java
@@ -1,106 +1,110 @@
 package profilosistema;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
 import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 public class ModificaEmailTest {
   private WebDriver driver;
-  private Map<String, Object> vars;
   JavascriptExecutor js;
   @Before
   public void setUp() {
-	  System.setProperty("webdriver.chrome.driver","test/materialesistema/chromedriver");
-	 // System.setProperty("webdriver.chrome.driver","test/profilosistema/chromedriver.exe");
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
+      //System.setProperty("webdriver.chrome.driver","test/materialesistema/chromedriver");
+      System.setProperty("webdriver.chrome.driver","test/profilosistema/chromedriver.exe");
+      driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
+      js = (JavascriptExecutor) driver;
   }
   @After
   public void tearDown() {
     driver.quit();
   }
+
+  @Test
+  public void testModificaEmailOK() {
+    driver.get("http://localhost:8080/SocialNotes/");
+    driver.manage().window().setSize(new Dimension(1382, 736));
+    driver.findElement(By.linkText("Accedi")).click();
+    driver.findElement(By.id("inputEmail")).sendKeys("rock1");
+    driver.findElement(By.id("inputPassword")).click();
+    driver.findElement(By.id("inputPassword")).sendKeys("Mensa0");
+    driver.findElement(By.cssSelector(".btn")).click();
+    driver.findElement(By.id("navbarDropdown")).click();
+    driver.findElement(By.linkText("Modifica profilo")).click();
+    driver.findElement(By.cssSelector(".editable-main-row .grid__item > .btn")).click();
+    driver.findElement(By.name("mail")).click();
+    driver.findElement(By.name("mail")).sendKeys("rocco.iuliano65@gmail.com");
+    driver.findElement(By.cssSelector(".editable-main-row > .hidden-form")).click();
+    driver.findElement(By.cssSelector(".one-third:nth-child(4) .btn")).click();
+    driver.findElement(By.cssSelector(".btn--primary")).click();
+    assertThat(driver.getTitle(), is("SocialNotes"));
+    driver.close();
+  }
+
   @Test
   public void testModificaEmailEsistente() {
-    driver.get("http://127.0.0.1:8080/SocialNotes/");
-    driver.manage().window().setSize(new Dimension(1936, 1056));
+    driver.get("http://localhost:8080/SocialNotes/");
+    driver.manage().window().setSize(new Dimension(1366, 720));
     driver.findElement(By.linkText("Accedi")).click();
+    driver.findElement(By.id("inputEmail")).click();
     driver.findElement(By.id("inputEmail")).sendKeys("califano03");
-    driver.findElement(By.id("inputPassword")).sendKeys("Alfonso32!");
-    driver.findElement(By.id("inputPassword")).sendKeys(Keys.ENTER);
-    driver.findElement(By.cssSelector(".bi-person-lines-fill > path")).click();
+    driver.findElement(By.id("inputPassword")).sendKeys("Califano03");
+    driver.findElement(By.cssSelector(".btn")).click();
+    driver.findElement(By.id("navbarDropdown")).click();
     driver.findElement(By.linkText("Modifica profilo")).click();
     driver.findElement(By.cssSelector(".editable-main-row .grid__item > .btn")).click();
     driver.findElement(By.name("mail")).click();
-    driver.findElement(By.name("mail")).sendKeys("faj320@gmail.com");
-    driver.findElement(By.cssSelector(".one-third:nth-child(4) > .submit")).click();
+    driver.findElement(By.name("mail")).sendKeys("acalifano123@gmail.com");
+    driver.findElement(By.cssSelector(".editable-main-row > .hidden-form")).click();
+    assertThat(driver.findElement(By.id("email-feedback")).getText(), is("L'indirizzo scelto è già esistente."));
     {
-      List<WebElement> elements = driver.findElements(By.id("email-feedback"));
-      assert(elements.size() > 0);
-    }
-  }
-  @Test
-  public void testModificaEmailFormatoNonCorretto() {
-    driver.get("http://127.0.0.1:8080/SocialNotes/");
-    driver.manage().window().setSize(new Dimension(1936, 1056));
-    driver.findElement(By.linkText("Accedi")).click();
-    driver.findElement(By.id("inputEmail")).sendKeys("califano03");
-    driver.findElement(By.id("inputPassword")).click();
-    driver.findElement(By.id("inputPassword")).sendKeys("Alfonso32!");
-    driver.findElement(By.id("inputPassword")).sendKeys(Keys.ENTER);
-    driver.findElement(By.cssSelector(".bi-person-lines-fill")).click();
-    driver.findElement(By.linkText("Modifica profilo")).click();
-    driver.findElement(By.cssSelector(".editable-main-row .grid__item > .btn")).click();
-    driver.findElement(By.name("mail")).click();
-    driver.findElement(By.name("mail")).sendKeys("acalifano123gmail.com");
-    {
-      WebElement element = driver.findElement(By.cssSelector(".one-third:nth-child(4) > .submit"));
+      WebElement element = driver.findElement(By.cssSelector(".btn--primary"));
       Actions builder = new Actions(driver);
       builder.moveToElement(element).clickAndHold().perform();
     }
     {
-      WebElement element = driver.findElement(By.cssSelector(".one-third:nth-child(4)"));
+      WebElement element = driver.findElement(By.cssSelector(".one-half:nth-child(4)"));
       Actions builder = new Actions(driver);
       builder.moveToElement(element).release().perform();
     }
-    driver.findElement(By.cssSelector(".one-third:nth-child(4)")).click();
-    assertThat(driver.findElement(By.id("email-feedback")).getText(), is("Per favore inserisci un indirizzo Email valido."));
-  }
-  @Test
-  public void testModificaEmailNonEsistente() {
-    driver.get("http://127.0.0.1:8080/SocialNotes/");
-    driver.manage().window().setSize(new Dimension(1936, 1056));
-    driver.findElement(By.linkText("Accedi")).click();
-    driver.findElement(By.id("inputEmail")).sendKeys("califano03");
-    driver.findElement(By.id("inputPassword")).sendKeys("Alfonso32!");
-    driver.findElement(By.id("inputPassword")).sendKeys(Keys.ENTER);
-    driver.findElement(By.cssSelector(".bi-person-lines-fill > path")).click();
-    driver.findElement(By.linkText("Modifica profilo")).click();
-    driver.findElement(By.cssSelector(".editable-main-row .grid__item > .btn")).click();
-    driver.findElement(By.name("mail")).click();
-    driver.findElement(By.name("mail")).sendKeys("fffffffffffff@hotmail.com");
+    driver.findElement(By.cssSelector(".one-half:nth-child(4)")).click();
     driver.findElement(By.cssSelector(".btn--primary")).click();
     {
       List<WebElement> elements = driver.findElements(By.cssSelector(".alert"));
-      assert(elements.size() > 0);
+      Assert.assertEquals(elements.size(),0);
     }
+    driver.findElement(By.linkText("Logout")).click();
+    driver.close();
   }
+  @Test
+  public void testModificaEmailFormatoNonCorretto() {
+    driver.get("http://localhost:8080/SocialNotes/");
+    driver.manage().window().setSize(new Dimension(1382, 736));
+    driver.findElement(By.linkText("Accedi")).click();
+    driver.findElement(By.id("inputEmail")).sendKeys("califano03");
+    driver.findElement(By.id("inputPassword")).click();
+    driver.findElement(By.id("inputPassword")).sendKeys("Califano03");
+    driver.findElement(By.cssSelector(".btn")).click();
+    driver.findElement(By.id("navbarDropdown")).click();
+    driver.findElement(By.linkText("Modifica profilo")).click();
+    driver.findElement(By.cssSelector(".editable-main-row .grid__item > .btn")).click();
+    driver.findElement(By.name("mail")).click();
+    driver.findElement(By.name("mail")).sendKeys("acalifano123gmail.com");
+    driver.findElement(By.cssSelector(".one-third > h1")).click();
+    assertThat(driver.findElement(By.id("email-feedback")).getText(), is("Per favore inserisci un indirizzo Email valido."));
+    driver.findElement(By.linkText("Logout")).click();
+    driver.close();
+  }
+
 }
