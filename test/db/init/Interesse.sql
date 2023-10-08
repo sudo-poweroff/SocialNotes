4141add68ef5d696982e5358b76e0866abbd2b63
sudo-poweroff <sdellaporta34@gmail.com>
2023-10-08 19:09:03 +0200

    [CR5] Corretti errori in Interesse.sql e nei relativi file xml #5

--- test/db/init/Interesse.sql
+++ test/db/init/Interesse.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS Corso(
-    `Username` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS Interesse(
+    `Username` varchar(30) NOT NULL,
     `CodiceCorso` int NOT NULL,
     PRIMARY KEY (`Username`,`CodiceCorso`)
 );
