d88a2fe7cf3dc2f9c79caf3a9c67e5e30c828ee6
sudo-poweroff <sdellaporta34@gmail.com>
2023-10-18 11:36:27 +0200

    [CR5]Aggiunta la tabella materiale in Interesse.sql e aggiunte le istanze di materiale in Interesse.xml #5

--- test/db/init/Interesse.sql
+++ test/db/init/Interesse.sql
@@ -12,3 +12,16 @@ CREATE TABLE IF NOT EXISTS Corso (
     `Denominazione` varchar(300) DEFAULT NULL,
     PRIMARY KEY (`CodiceCorso`)
 );
+
+CREATE TABLE IF NOT EXISTS Materiale (
+    `CodiceMateriale` int NOT NULL AUTO_INCREMENT,
+    `DataCaricamento` date NOT NULL,
+    `Keywords` longtext,
+    `Costo` int DEFAULT NULL,
+    `Descrizione` varchar(100) NOT NULL,
+    `Hidden` BOOLEAN NOT NULL,
+    `CodiceCorso` int NOT NULL,
+    `Username` varchar(30) DEFAULT NULL,
+    `nomeFile` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`CodiceMateriale`)
+    );
