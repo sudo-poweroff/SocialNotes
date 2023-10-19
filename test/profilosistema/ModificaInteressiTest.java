bfe8e51fe62c7f9027fe8a1541fb9bdeb272be9d
GGFlutterdev <luigi.allocca12345@live.it>
2023-10-19 13:40:15 +0200

    Aggiunte classi di test

--- test/profilosistema/ModificaInteressiTest.java
+++ test/profilosistema/ModificaInteressiTest.java
@@ -32,17 +32,5 @@ public class ModificaInteressiTest {
     @Test
     public void testAggiungiInteressi() {
         driver.get("http://127.0.0.1:8080/SocialNotes/");
-        driver.manage().window().setSize(new Dimension(1936, 1056));
-        driver.findElement(By.linkText("Accedi")).click();
-        driver.findElement(By.id("inputEmail")).sendKeys("califano03");
-        driver.findElement(By.id("inputPassword")).click();
-        driver.findElement(By.id("inputPassword")).sendKeys("Alfonso32!");
-        driver.findElement(By.id("inputPassword")).sendKeys(Keys.ENTER);
-        driver.findElement(By.cssSelector(".bi-person-lines-fill > path")).click();
-        driver.findElement(By.linkText("Modifica profilo")).click();
-        driver.findElement(By.cssSelector(".editable-main-row .grid__item > .btn")).click();
-        driver.findElement(By.cssSelector("p:nth-child(1)")).click();
-        driver.findElement(By.id("picture")).sendKeys("C:\\Users\\PC\\Downloads\\Lezione03_06_2020.pdf");
-        assertThat(driver.switchTo().alert().getText(), is("Tipo del file non valido"));
     }
 }
