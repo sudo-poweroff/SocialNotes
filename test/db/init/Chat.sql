ad0cfedfdd2d3ca17d3266daf52f9c4d2515f24d
sudo-poweroff <sdellaporta34@gmail.com>
2023-10-24 16:30:47 +0200

    Modificata la tabella utente in Chat.sql e modificati i relativi file xml

--- test/db/init/Chat.sql
+++ test/db/init/Chat.sql
@@ -9,17 +9,19 @@ CREATE TABLE IF NOT EXISTS Partecipazione(
   PRIMARY KEY (`Username`,`ChatID`)
 );
 CREATE TABLE IF NOT EXISTS `Utente` (
-  `Username` varchar(30) NOT NULL,
-  `Nome` varchar(30) NOT NULL,
-  `Cognome` varchar(30) NOT NULL,
-  `Img` blob DEFAULT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Pass` varchar(30) NOT NULL,
-  `DataNascita` date DEFAULT NULL,
-  `Coin` int DEFAULT NULL,
-  `Ban` date DEFAULT NULL,
-  `Denominazione` varchar(300) DEFAULT NULL,
-  `dipName` varchar(400) DEFAULT NULL,
-  `Ruolo` int NOT NULL,
-  PRIMARY KEY (`Username`)
-);
+    `Username` varchar(30) NOT NULL,
+    `Nome` varchar(30) NOT NULL,
+    `Cognome` varchar(30) NOT NULL,
+    `Img` blob DEFAULT NULL,
+    `Email` varchar(50) NOT NULL,
+    `Pass` varchar(30) NOT NULL,
+    `DataNascita` date DEFAULT NULL,
+    `Coin` int DEFAULT NULL,
+    `Ban` date DEFAULT NULL,
+    `Denominazione` varchar(300) DEFAULT NULL,
+    `dipName` varchar(400) DEFAULT NULL,
+    `Ruolo` int NOT NULL,
+    `Bloccato` datetime DEFAULT NULL,
+    `Verificato` BOOLEAN NOT NULL,
+    PRIMARY KEY (`Username`)
+    );
