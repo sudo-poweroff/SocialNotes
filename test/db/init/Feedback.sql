ef4cc101144a8353e2bfb70d4b0a656412cc1c4f
Rocco000 <r.iuliano13@studenti.unisa.it>
2023-10-24 16:25:04 +0200

    Aggiornato la tabella Materiale e Utente rispetto alle CR effettuate

--- test/db/init/Feedback.sql
+++ test/db/init/Feedback.sql
@@ -10,33 +10,34 @@ CREATE TABLE IF NOT EXISTS Feedback (
 
 
 CREATE TABLE IF NOT EXISTS `Utente` (
-  `Username` varchar(30) NOT NULL,
-  `Nome` varchar(30) NOT NULL,
-  `Cognome` varchar(30) NOT NULL,
-  `Img` blob DEFAULT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Pass` varchar(30) NOT NULL,
-  `DataNascita` date DEFAULT NULL,
-  `Coin` int DEFAULT NULL,
-  `Ban` date DEFAULT NULL,
-  `Denominazione` varchar(300) DEFAULT NULL,
-  `dipName` varchar(400) DEFAULT NULL,
-  `Ruolo` int NOT NULL,
-  PRIMARY KEY (`Username`)
+    `Username` varchar(30) NOT NULL,
+    `Nome` varchar(30) NOT NULL,
+    `Cognome` varchar(30) NOT NULL,
+    `Img` blob DEFAULT NULL,
+    `Email` varchar(50) NOT NULL,
+    `Pass` varchar(30) NOT NULL,
+    `DataNascita` date DEFAULT NULL,
+    `Coin` int DEFAULT NULL,
+    `Ban` date DEFAULT NULL,
+    `Denominazione` varchar(300) DEFAULT NULL,
+    `dipName` varchar(400) DEFAULT NULL,
+    `Ruolo` int NOT NULL,
+    `Bloccato` datetime DEFAULT NULL,
+    `Verificato` BOOLEAN NOT NULL,
+    PRIMARY KEY (`Username`)
 );
 
 CREATE TABLE IF NOT EXISTS Materiale (
-  `CodiceMateriale` int NOT NULL AUTO_INCREMENT,
-  `DataCaricamento` date NOT NULL,
-  `Keywords` longtext DEFAULT NULL,
-  `Costo` int DEFAULT NULL,
-  `Descrizione` varchar(100) NOT NULL,
-  `Hidden` int NOT NULL,
-  `CodiceCorso` int NOT NULL,
-  `Username` varchar(30) DEFAULT NULL,
-  `Anteprima` longblob NOT NULL,
-  `IdFile` int DEFAULT NULL,
-  PRIMARY KEY (`CodiceMateriale`)
+    `CodiceMateriale` int NOT NULL AUTO_INCREMENT,
+    `DataCaricamento` date NOT NULL,
+    `Keywords` longtext,
+    `Costo` int DEFAULT NULL,
+    `Descrizione` varchar(100) NOT NULL,
+    `Hidden` BOOLEAN NOT NULL,
+    `CodiceCorso` int NOT NULL,
+    `Username` varchar(30) DEFAULT NULL,
+    `nomeFile` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`CodiceMateriale`)
 );
 
 
