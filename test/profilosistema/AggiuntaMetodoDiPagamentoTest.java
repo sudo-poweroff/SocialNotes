3df3c4609e8390b70122558f7d89b9c81a2227b4
sudo-poweroff <sdellaporta34@gmail.com>
2023-11-01 12:54:14 +0100

    Risolto errore selenium css-selector

--- test/profilosistema/AggiuntaMetodoDiPagamentoTest.java
+++ test/profilosistema/AggiuntaMetodoDiPagamentoTest.java
@@ -2,38 +2,26 @@ package profilosistema;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.Keys;
 import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 public class AggiuntaMetodoDiPagamentoTest {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
   @Before
   public void setUp() {
-	  System.setProperty("webdriver.chrome.driver","test/materialesistema/chromedriver");
-	  //System.setProperty("webdriver.chrome.driver","test/profilosistema/chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver","test/materialesistema/chromedriver.exe");
+    //System.setProperty("webdriver.chrome.driver","test/materialesistema/chromedriver");
     driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
     js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
   }
   @After
   public void tearDown() {
@@ -176,7 +164,7 @@ public class AggiuntaMetodoDiPagamentoTest {
     driver.findElement(By.id("inputEmail")).sendKeys("califano03");
     driver.findElement(By.id("inputPassword")).sendKeys("Califano03");
     driver.findElement(By.id("inputPassword")).sendKeys(Keys.ENTER);
-    driver.findElement(By.cssSelector(".bi-person-lines-fill > path")).click();
+    driver.findElement(By.cssSelector(".bi-person-lines-fill")).click();
     driver.findElement(By.linkText("Modifica profilo")).click();
     driver.findElement(By.cssSelector(".row-group:nth-child(4) .grid__item > .btn")).click();
     driver.findElement(By.name("nomecarta")).click();
@@ -234,7 +222,7 @@ public class AggiuntaMetodoDiPagamentoTest {
     driver.findElement(By.id("inputEmail")).sendKeys("califano03");
     driver.findElement(By.id("inputPassword")).sendKeys("Califano03");
     driver.findElement(By.id("inputPassword")).sendKeys(Keys.ENTER);
-    driver.findElement(By.cssSelector(".bi-person-lines-fill > path")).click();
+    driver.findElement(By.cssSelector(".bi-person-lines-fill")).click();
     driver.findElement(By.linkText("Modifica profilo")).click();
     driver.findElement(By.cssSelector(".row-group:nth-child(4) .grid__item > .btn")).click();
     driver.findElement(By.name("nomecarta")).click();
@@ -306,7 +294,6 @@ public class AggiuntaMetodoDiPagamentoTest {
     }
     driver.findElement(By.cssSelector(".form-group:nth-child(2)")).click();
     driver.findElement(By.name("numcarta")).sendKeys("9999999999991111");
-    driver.findElement(By.name("numcarta")).sendKeys("9999999999991112");
     {
       WebElement element = driver.findElement(By.cssSelector(".form-group:nth-child(2) .help-block"));
       Actions builder = new Actions(driver);
@@ -345,9 +332,8 @@ public class AggiuntaMetodoDiPagamentoTest {
     driver.findElement(By.linkText("Accedi")).click();
     driver.findElement(By.id("inputEmail")).sendKeys("califano03");
     driver.findElement(By.id("inputPassword")).sendKeys("Califano03");
-    driver.findElement(By.id("inputPassword")).sendKeys("Califano03");
     driver.findElement(By.id("inputPassword")).sendKeys(Keys.ENTER);
-    driver.findElement(By.cssSelector(".bi-person-lines-fill > path")).click();
+    driver.findElement(By.cssSelector(".bi-person-lines-fill")).click();
     driver.findElement(By.linkText("Modifica profilo")).click();
     driver.findElement(By.cssSelector(".row-group:nth-child(4) .grid__item > .btn")).click();
     driver.findElement(By.name("nomecarta")).click();
--- test/profilosistema/EliminazioneMetodoDiPagamentoTest.java
+++ test/profilosistema/EliminazioneMetodoDiPagamentoTest.java
@@ -2,38 +2,26 @@ package profilosistema;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.Keys;
 import java.util.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 public class EliminazioneMetodoDiPagamentoTest {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
   @Before
   public void setUp() {
-	  System.setProperty("webdriver.chrome.driver","test/materialesistema/chromedriver");
-	  //System.setProperty("webdriver.chrome.driver","test/profilosistema/chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver","test/materialesistema/chromedriver.exe");
+    //System.setProperty("webdriver.chrome.driver","test/materialesistema/chromedriver");
     driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
     js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
   }
   @After
   public void tearDown() {
@@ -101,9 +89,7 @@ public class EliminazioneMetodoDiPagamentoTest {
     driver.findElement(By.id("inputPassword")).click();
     driver.findElement(By.id("inputPassword")).sendKeys("Califano03");
     driver.findElement(By.id("inputPassword")).sendKeys(Keys.ENTER);
-    driver.findElement(By.cssSelector(".bi-person-lines-fill > path")).click();
-    driver.findElement(By.cssSelector(".bi-person-lines-fill > path")).click();
-    driver.findElement(By.cssSelector(".bi-person-lines-fill > path")).click();
+    driver.findElement(By.cssSelector(".bi-person-lines-fill")).click();
     driver.findElement(By.linkText("Modifica profilo")).click();
     driver.findElement(By.cssSelector(".row-group:nth-child(5) .grid__item > .btn")).click();
     driver.findElement(By.name("numcartaDelete")).click();
